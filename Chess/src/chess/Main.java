f947f5dc91d5ddb1a098206604610378ee281538
theCuriosityEnthusisast <mgmayhem76@gmail.com>
2015-07-29 20:44:47 +0530

    Initial Commit

---
+++ Chess/src/chess/Main.java
@@ -0,0 +1,14 @@
+package chess;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
+		String str = "d4 d5";
+		System.out.println(str);
+		input.close();
+	}
+}
