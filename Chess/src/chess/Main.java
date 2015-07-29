101b058e206bb7cdd6104139cde3e5e8467285cb
theCuriosityEnthusisast <mgmayhem76@gmail.com>
2015-07-29 21:07:50 +0530

    Check

--- Chess/src/chess/Main.java
+++ Chess/src/chess/Main.java
@@ -8,7 +8,7 @@ public class Main {
 		// TODO Auto-generated method stub
 		Scanner input = new Scanner(System.in);
 		String str = "d4 d5";
-		System.out.println("The move is that OI: " +str );
+		System.out.println("The move is: " +str );
 		input.close();
 	}
 }
