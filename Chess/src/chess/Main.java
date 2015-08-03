84b7888e09749ceb2150c19822f5107ad1a0781b
theCuriosityEnthusisast <mgmayhem76@gmail.com>
2015-08-03 16:35:37 +0530

    Minor changes to main, two new objects created.

--- Chess/src/chess/Main.java
+++ Chess/src/chess/Main.java
@@ -7,11 +7,14 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner input = new Scanner(System.in);
-		//String str = "d4 d5";
+		String str = "d4 d5";
 		//System.out.println("The move is : " +str );
 		input.close();
 		
 		ChessBoard cb = new ChessBoard();
 		cb.printBoard();
+		
+		Move move = new Move();
+		move.decodeMove(str);
 	}
 }
