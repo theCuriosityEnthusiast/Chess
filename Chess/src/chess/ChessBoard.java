489e86b806e57bf049efdaf3b1d95d3e3cb27516
theCuriosityEnthusisast <mgmayhem76@gmail.com>
2015-08-03 16:17:32 +0530

    Added a new Class Move to the project

--- Chess/src/chess/ChessBoard.java
+++ Chess/src/chess/ChessBoard.java
@@ -4,18 +4,18 @@ public class ChessBoard {
 
 	String chessboard[][];
 	
-	final String[] piecesRepresentation = {"R1","N1", "B1", "K ", "Q ", "B2", "N2", "R2"};
+	final String[] piecesRepresentation = {"   ", "R1","N1", "B1", "K ", "Q ", "B2", "N2", "R2"};
 	int lengthOfBoard;
 	
 	ChessBoard(){
 		lengthOfBoard = 9;
 		chessboard = new String[lengthOfBoard][lengthOfBoard];
 		
-		for(int i=0; i<lengthOfBoard; i++){
-			chessboard[i][1] = "W" + piecesRepresentation[i];
-			chessboard[i][8] = "B" + piecesRepresentation[i];
-			chessboard[i][1+1] = "W" + "P" + i;
-			chessboard[i][8-1] = "B" + "P" + i;
+		for(int i=1; i<lengthOfBoard; i++){
+			chessboard[1][i] = "W" + piecesRepresentation[i];
+			chessboard[8][i] = "B" + piecesRepresentation[i];
+			chessboard[1+1][i] = "W" + "P" + i;
+			chessboard[8-1][i] = "B" + "P" + i;
 		}
 	}
 	
@@ -28,10 +28,11 @@ public class ChessBoard {
 		for(int i=lengthOfBoard-1; i > 0; i--){
 			for(int j=1; j<lengthOfBoard; j++){
 				if(chessboard[i][j] != null){
-					System.out.print("   ");
+					System.out.print(chessboard[i][j] + " ");
+					
 				}
 				else {
-					System.out.print("   ");
+					System.out.print("    ");
 				}
 			}
 			System.out.println();
---
+++ Chess/src/chess/Move.java
@@ -0,0 +1,16 @@
+package chess;
+
+public class Move {
+	
+	private int currentPlayer;
+	
+	Move(){
+		currentPlayer = 0;
+	}
+
+	public void decodeMove(String moves){
+		
+		String[] twoMoves = moves.split(" ");
+		
+	}
+}
